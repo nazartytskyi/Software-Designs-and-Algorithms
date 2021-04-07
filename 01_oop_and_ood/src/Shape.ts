6c44703e01b41a9cbde586e4544ad01d3eca3f30
Nazar Tytskyi <Nazar_Tytskyi@epam.com>
2021-04-07 14:28:09 +0300

    HW1 implemented

---
+++ 01_oop_and_ood/src/Shape.ts
@@ -0,0 +1,42 @@
+import { Point } from "./Point";
+
+export abstract class Shape {
+  protected color: string;
+  protected filled: boolean;
+  protected points: Point[];
+
+  constructor(points: Point[]);
+  constructor(points: Point[], color: string, filled: boolean);
+
+  constructor(
+    points: Point[],
+    color: string = "green",
+    filled: boolean = true
+  ) {
+    if (points.length < 3) {
+      throw new Error("At lease 3 points are required");
+    }
+
+    this.points = points;
+    this.color = color;
+    this.filled = filled;
+  }
+
+  toString(): string {
+    const points = this.points.map((point) => point.toString());
+
+    return `A Shape with color of ${this.color} and ${
+      this.filled ? "filled" : "not filled"
+    }. Points: ${points.join(", ")}.`;
+  }
+
+  getPerimeter() {
+    return this.points.reduce((acc: number, point: Point, i: number) => {
+      const index = i ? i : this.points.length;
+
+      return acc + point.distance(this.points[index - 1]);
+    }, 0);
+  }
+
+  abstract getType(): string;
+}
---
+++ 01_oop_and_ood/src/Triangle.ts
@@ -0,0 +1,49 @@
+import { Point } from './Point';
+import { Shape } from './Shape';
+
+export class Triangle extends Shape {
+    constructor(point1: Point, point2: Point, point3: Point);
+    constructor(point1: Point, point2: Point, point3: Point, color: string, filled: boolean);
+    
+    constructor(point1: Point, point2: Point, point3: Point, color: string = 'green', filled: boolean = false) {
+        super([point1, point2, point3], color, filled);
+    }
+
+    toString(): string {
+        const points = this.points.map((point, i) => `v${i + 1}=${point.toString()}`);
+
+        return `Triangle[${points.join(',')}]`;
+    }
+
+    getType(): string {
+        if (this.isEquilateral()) {
+            return 'equilateral triangle';
+        }
+
+        if (this.isIsosceles()) {
+            return 'isosceles triangle'
+        }
+
+        return 'scalene triangle'
+    }
+
+    private getSidesLengths(): number[] {
+        return this.points.map((point, i) => {
+            const index = i ? i : this.points.length;
+
+            return point.distance(this.points[index - 1]);
+        })
+    }
+
+    private isEquilateral() {
+        const sideLengths = this.getSidesLengths();
+
+        return sideLengths.every(length => length === sideLengths[0]);
+    }
+
+    private isIsosceles() {
+        const sideLengths = this.getSidesLengths();
+
+        return sideLengths[0] === sideLengths[1] || sideLengths[0] === sideLengths[2] || sideLengths[1] === sideLengths[2];
+    }
+}
